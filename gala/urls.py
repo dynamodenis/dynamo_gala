75e06e0ac904dcc1c878ef22b0976c840193a142
dynamodenis <dmbugua66@gmail.com>
2020-05-24 13:04:39 +0300

    designed the images

--- gala/urls.py
+++ gala/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/',views.search_category,name='search_category'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- gala/views.py
+++ gala/views.py
@@ -6,3 +6,15 @@ from .models import Image,Location,Category
 def index(request):
     images=Image.objects.all()
     return render(request,'gala/index.html',{'images':images})
+
+def search_category(request):
+    if 'category' in request.GET and request.GET['category']:
+        search_term=request.GET.get('category')
+        searched_category=Image.objects.filter(category=search_term)
+        message=f'{search_term}'
+        
+        return render(request,'gala/search_category.html',{'message':message,'searched':search_category,'title':search_term})
+    
+    else:
+        message=f"You haven't searched for anything."
+        return render(request,'gala/search_category.html',{'message':message})
