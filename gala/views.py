785008e9544edb7632b4cb3a3b8a2220e830544c
dynamodenis <dmbugua66@gmail.com>
2020-05-24 15:39:35 +0300

    added a location and date in image

--- gala/views.py
+++ gala/views.py
@@ -11,9 +11,10 @@ def search_category(request):
     if 'category' in request.GET and request.GET['category']:
         search_term=request.GET.get('category')
         searched_category=Image.objects.filter(category=search_term)
+        print(searched_category)
         message=f'{search_term}'
         
-        return render(request,'gala/search_category.html',{'message':message,'searched':search_category,'title':search_term})
+        return render(request,'gala/search_category.html',{'message':message,'searched':searched_category,'title':search_term})
     
     else:
         message=f"You haven't searched for anything."
